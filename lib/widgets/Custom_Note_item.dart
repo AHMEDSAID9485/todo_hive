f44945bd64d321e4769ddee8b547989ed8135d73
AHMEDSAID9485 <ahmedascahmed@gmail.com>
2026-09-02 09:09:03 +0300

    edit on note item

--- lib/widgets/Custom_Note_item.dart
+++ lib/widgets/Custom_Note_item.dart
@@ -5,8 +5,8 @@ import 'package:todo_hive/core/constant/App_color.dart';
 
 class CustomNoteitem extends StatelessWidget {
   const CustomNoteitem({super.key, this.color, this.onTap});
-final Color? color;
-final void Function()? onTap;
+  final Color? color;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -15,24 +15,44 @@ final void Function()? onTap;
         padding: EdgeInsets.all(25),
         decoration: BoxDecoration(
           color: color,
-          borderRadius: BorderRadius.circular(16)
+          borderRadius: BorderRadius.circular(12),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Flutter Tips',style: TextStyle(fontSize: 24,color: AppColor.blackcolor),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Flutter Tips',
+                  style: TextStyle(fontSize: 24, color: AppColor.blackcolor),
+                ),
+                Icon(Icons.delete, size: 35, color: AppColor.blackcolor),
+              ],
+            ),
+            Gap(18),
+            Text(
+              'Build Your Craear with Ahmed Saeed',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                color: AppColor.thirdcolor,
+              ),
+            ),
+            Gap(15),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Icon(Icons.delete,size: 40,color: AppColor.blackcolor,)
-              ],
-            ),
-            Text('Build Your Craear with Ahmed Saeed',maxLines: 2,overflow: TextOverflow.ellipsis,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: AppColor.thirdcolor),),
-             Gap(10),
-             Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-              Text('${DateFormat('MMM dd,yyyy').format(DateTime.now())}',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15,color: AppColor.blackcolor),),
+                Text(
+                  '${DateFormat('MMM dd,yyyy').format(DateTime.now())}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: AppColor.blackcolor,
+                  ),
+                ),
               ],
             ),
           ],
