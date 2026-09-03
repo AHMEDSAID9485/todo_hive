488cc8e04e9f68cc2186ccf00295ad7633c5fd14
AHMEDSAID9485 <ahmedascahmed@gmail.com>
2026-09-03 23:05:16 +0300

    add divider for bottom sheet

--- lib/widgets/AddNoteBottemSheet.dart
+++ lib/widgets/AddNoteBottemSheet.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:todo_hive/core/constant/App_color.dart';
 import 'package:todo_hive/widgets/CustomBottom.dart';
 import 'package:todo_hive/widgets/CustomTextField.dart';
 
@@ -15,18 +16,18 @@ class AddNoteBottemSheet extends StatelessWidget {
       padding:  EdgeInsets.only(
         left: 16,
         right: 16,
-        top: 16,
         bottom: MediaQuery.of(context).viewInsets.bottom+16
       ),
       child: SingleChildScrollView(
         child: Column(
           children: [
-            Gap(15),
-            CustomTextField(hint: 'Title'),
-            Gap(15),
-            CustomTextField(hint: 'Content',maxlines: 5,),
-            Gap(20),
-            CustomBottom(title: 'ADD',)
+           Divider(color:AppColor.witecolor,thickness: 4,indent: 150,endIndent: 150,radius:  BorderRadius.circular(1),),
+           const Gap(15),
+           const CustomTextField(hint: 'Title'),
+           const Gap(15),
+           const CustomTextField(hint: 'Content',maxlines: 5,),
+           const Gap(20),
+           const CustomBottom(title: 'ADD',)
           ],
         ),
       ),
